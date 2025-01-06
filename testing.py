530742f97884c022188cd7ef553641510a6bc52b
Ubuntu <ubuntu@ip-172-31-16-187.ec2.internal>
2025-01-06 18:48:23 +0000

    modified python file

--- testing.py
+++ testing.py
@@ -1,1 +1,6 @@
-print("Hello Python World!")
+# a python program that will sum two numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(f"Sum of two numbers: {num1 + num2}")
